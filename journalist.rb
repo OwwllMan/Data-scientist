be9b5dc5035a4baa695cd1a373dde00e1096ed07
Mathieu D <mathieu@MacBook-Air-de-Mathieu.local>
2018-10-04 17:07:33 +0200

    Manque readMe et vérif

---
+++ journalist.rb
@@ -0,0 +1,77 @@
+handle = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def countHandleWithNumber(handle)#Méthode qui va compter le nombre de pseudo avec un nombre/chiffre
+    nbHandle = 0
+    handle.each do |user|#On séléctionne chaque pseudo
+      number = 0
+      user.split("").each do |char|#On séléctionne chaque caractère de chaque pseudo
+        if char >= "0" and char <= "9"#Si le caractère du pseudo contient un nombre sup ou égale à 0 ou inf ou égale à 9
+          number = 1
+        end
+      end
+      if number == 1
+        nbHandle += 1
+      end
+    end
+    return nbHandle
+end
+
+def audeHandle(handle)#Méthode qui va compter le nombre de pseudo avec qui contiennent les lettre "aude"
+    nbAudeHandle = 0
+    handle.each do |user|
+        if (user.downcase).include?("aude")#Si l'élément en minuscule inclue les lettres "aude" incrémente  nbAudeHandle
+            nbAudeHandle +=1
+        end
+    end 
+    return nbAudeHandle
+end
+
+def upcaseHandle(handle)#Méthode qui va compter le nb de pseudo avec la première lettre en majuscule 
+    nbupcaseHandle = 0
+    handle.each do |user| #Chaque élément de l'array "handle" ici appelé user
+        if user[1] == user[1].upcase#Si le deuxième caratère de l'élément est en majuscule 
+            nbupcaseHandle += 1#On incrémente le nombre total de pseudo avec une majuscule
+        end
+    end 
+    return nbupcaseHandle#On affecte la valeur de nbupcaseHandle à la méthode
+end
+
+def handleWithUpcase(handle)#Méthode qui va compter le nb de pseudo avec une lettre majuscule à l'intérieur 
+  nbHandleUpcase = 0
+  handle.each do |user|
+    number = 0
+    count = 0
+    user.split("").each do |char|
+      if count != 0
+        if char == char.upcase && ((char >= "A" && char <= "Z") || (char >= "a" && char <= "z"))
+          number = 1
+        end
+      end
+      count += 1
+    end
+    if number == 1
+      nbHandleUpcase += 1
+    end
+  end
+  return nbHandleUpcase
+end
+
+def underscoreInHandle(handle)#Méthode qui va compter le nb de pseudo avec un underscore à l'intérieur 
+  nbUnderscore = 0# Le nombre d'élément avec un "_"
+  handle.each do |user|#Chaque élément de l'array "handle" ici appelé user
+    user.split("").each do |char|
+      if char == "_"
+        nbUnderscore += 1
+      end
+    end
+  end
+  return nbUnderscore#On affecte le la valeur de nbUnderlineHandle à la méthode underlineHandle
+end
+
+p "La liste contient le pseudo de  #{handle.length} journalistes"
+p "Il y a  #{countHandleWithNumber(handle)} journalistes qui on un chiffre/nombre dans leurs pseudo"
+p "La liste contient #{audeHandle(handle)} personne avec les lettre \"Aude\" dans leurs pseudo "
+p "Dans la liste il y a  #{upcaseHandle(handle)} personnes avec une majuscle à la première lettre de leurs dans leurs pseudo"
+p "Dans la liste il y a  un total #{handleWithUpcase(handle)} de personnes avec une ou plusieurs majuscule dans leurs pseudo"
+p "Dans la liste il y a  #{underscoreInHandle(handle)} personnes avec un underscore ('_') dans leurs pseudo"
+puts handle.sort_by{|word|word.downcase}
